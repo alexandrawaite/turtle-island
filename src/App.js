8963c5f3138783af24040d157f308e65173fd575
alexandrawaite <alexandrawaite@gmail.com>
2018-03-02 22:34:27 -0800

    Start building Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Navbar />
       </div>
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,14 @@ import './Navbar.css';
 class Navbar extends Component {
     render() {
       return(
-
+        <div className="navbar">
+          <div className="home-btn">
+            <p>Home</p>
+          </div>
+          <div className="about-btn">
+            <p>About</p>
+          </div>
+        </div>
       );
     }
 }
